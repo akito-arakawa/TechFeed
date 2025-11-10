3e2e22268192100c8ca64a1a4ff1956f9723723f
akito <167883398+akito-arakawa@users.noreply.github.com>
2025-11-10 11:55:51 +0900

    add:ArticleViewModelのリレーション定義

---
+++ src/app/Models/ArticleView.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ArticleView extends Model
+{
+    use HasFactory;
+
+    protected $table = 'article_views';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'user_id',
+        'article_id',
+        'session_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function article()
+    {
+        return $this->belongsTo(Article::class);
+    }
+}
