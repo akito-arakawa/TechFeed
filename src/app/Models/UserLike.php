45dd56949bb4bf78508ce844699fa40936e518f8
akito <167883398+akito-arakawa@users.noreply.github.com>
2025-11-10 13:51:52 +0900

    UserLikeModelのリレーション定義

---
+++ src/app/Models/UserLike.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserLike extends Model
+{
+    use HasFactory;
+
+    public $timestamps = true;
+    protected $table = 'user_likes';
+
+    protected $fillable = [
+        'user_id',
+        'article_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function article()
+    {
+        return $this->belongsTo(Article::class);
+    }
+    
+}
