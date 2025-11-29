bf55515adc7d9e1a712bd45b155348f4d57d5229
akito <167883398+akito-arakawa@users.noreply.github.com>
2025-11-30 00:15:19 +0900

    change:タイポがあったので修正

---
+++ src/database/migrations/2025_11_29_151028_rename_pubished_at_column.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->renameColumn('pubished_at', 'published_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
