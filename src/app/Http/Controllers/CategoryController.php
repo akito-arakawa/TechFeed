a5dd04f689d9fc7d727887fae13da47f1c356e9b
akito <167883398+akito-arakawa@users.noreply.github.com>
2025-12-28 18:48:58 +0900

    add:カテゴリ一覧表示APIを作成

---
+++ src/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        try {
+            return response()->json([
+                'categories' => CategoryResource::collection(Category::all()),
+            ]);
+        } catch (\Exception $e) {
+            return response([], 500);
+        }
+    }
+}
